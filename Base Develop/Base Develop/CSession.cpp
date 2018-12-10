ba0a61bcfb28bbe9226565cf58b2edc4a3623e0f
Mute(JunMin Kim) <j0803@naver.com>
2018-12-11 08:12:18 +0900

    revert

--- Base Develop/Base Develop/CSession.cpp
+++ Base Develop/Base Develop/CSession.cpp
@@ -4,26 +4,15 @@
 #include "TCPSocket.h"
 #include "UDPSocket.h"
 #include "SocketAddress.h"
-#include "MemoryStream.h"
-#include "InputMemoryStream.h"
-#include "OutputMemoryStream.h"
 
 CSession::CSession(std::shared_ptr<TCPSocket>& socket,
 	std::shared_ptr<SocketAddress>& address)
 {
 	_tcpSocket = socket;
 	_address = address;
-
-	_OutputStreamBuffer = std::make_shared<OutputMemoryStream>();
-	_InputStreamBuffer = std::make_shared<InputMemoryStream>();
 }
 
 
 CSession::~CSession()
 {
-}
-
-bool CSession::Recv()
-{
-	auto pStream = std::dynamic_pointer_cast<InputMemoryStream>(_InputStreamBuffer);
 }
