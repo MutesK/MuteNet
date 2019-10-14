c7ba28fbdf5f6ca8382a0413734292abd1b79370
Mute(JunMin Kim) <j0803@naver.com>
2019-10-14 17:49:19 +0900

    Update ServerHandleImpl.cpp

--- MuteNet/ServerHandleImpl.cpp
+++ MuteNet/ServerHandleImpl.cpp
@@ -2,6 +2,8 @@
 #include "ServerHandleImpl.h"
 #include "NetworkManager.h"
 #include "Acceptor.h"
+#include "SocketFunctionInvoker.h"
+#include "SocketUtil.h"
 
 namespace MuteNet
 {
@@ -37,6 +39,35 @@ namespace MuteNet
 			return false;
 
 		return true;
+	}
+
+	void ServerHandleImpl::Callback(intptr_t socket, sockaddr* address, int socklen, void* key)
+	{
+		ServerHandleImpl* Self = reinterpret_cast<ServerHandleImpl*>(key);
+
+		char IpAddress[128];
+		uint16_t port;
+
+		switch (address->sa_family)
+		{
+		case AF_INET:
+		{
+			sockaddr_in* sock_in = reinterpret_cast<sockaddr_in*>(address);
+			MuteNet::inet_ntop(AF_INET, &sock_in->sin_addr, IpAddress, 128);
+			port = htons(sock_in->sin_port);
+		}
+			break;
+		case AF_INET6:
+			break;
+		}
+
+		auto linkCallback = Self->_listenCallbacks->OnInComingConnection(IpAddress, port);
+
+		// Join Socket Ã³¸®
+		Self->_errorCode = SocketDelegateInvoker::Invoke(setsockopt, socket, SOL_SOCKET, SO_UPDATE_ACCEPT_CONTEXT,
+			(const char*)&listen, sizeof(SOCKET));
+	}
+
 	}
 
 
